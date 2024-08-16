2f70cd2cfe420ef6c7844d20e965fff70e39e95d
abushw03 <abushw03@yahoo.com>
2024-08-15 22:05:50 -0400

    updated cors

--- backend/app.js
+++ backend/app.js
@@ -11,12 +11,12 @@ const answerRoutes = require("./routes/answerRoute");
 const questionRoutes = require("./routes/questionRoute");
 
 // JSON middleware to extract JSON data
-app.use(express.json());
-const corsOptions = {
-  origin: "http://localhost:5173", // Replace with the allowed origin
-};
+// app.use(express.json());
+// const corsOptions = {
+//   origin: "http://localhost:5173", // Replace with the allowed origin
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 //user routes middleware file
